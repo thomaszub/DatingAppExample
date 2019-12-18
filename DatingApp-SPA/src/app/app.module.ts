8d9c083be8dc3313e9cf92bae443ff5022bb6211
thomaszub <thomas.zub@outlook.de>
2019-12-18 20:02:27 +0100

    Added register form

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { ValueComponent } from "./value/value.component";
+import { HomeComponent } from "./home/home.component";
 import { NavComponent } from "./nav/nav.component";
+import { RegisterComponent } from "./register/register.component";
 import { AuthService } from "./_services/auth.service";
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent, NavComponent],
+  declarations: [AppComponent, HomeComponent, NavComponent, RegisterComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -24,6 +24,7 @@ namespace DatingApp.API.Controllers
       _logger = logger;
     }
 
+    [AllowAnonymous]
     [HttpGet]
     public async Task<IActionResult> GetValues()
     {
@@ -31,6 +32,7 @@ namespace DatingApp.API.Controllers
       return Ok(values);
     }
 
+    [AllowAnonymous]
     [HttpGet("{id}")]
     public async Task<IActionResult> GetValue(int id) {
       var value = await _context.Values.FirstOrDefaultAsync(
