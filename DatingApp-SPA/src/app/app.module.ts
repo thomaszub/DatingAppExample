5d597456c88299ead4d414e900059fd30ce923f2
thomaszub <thomas.zub@outlook.de>
2019-12-20 20:49:44 +0100

    Added Dropdown from ngx-bootstrap

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { BsDropdownModule } from "ngx-bootstrap";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
@@ -12,7 +13,12 @@ import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NavComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
+  ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
 })
