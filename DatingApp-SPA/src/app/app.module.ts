573db8822fc074a78ac50867ffffac47d0623de3
thomaszub <thomas.zub@outlook.de>
2019-12-20 21:20:48 +0100

    Added routing and next components

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { BsDropdownModule } from "ngx-bootstrap";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
@@ -10,14 +11,27 @@ import { NavComponent } from "./nav/nav.component";
 import { RegisterComponent } from "./register/register.component";
 import { AuthService } from "./_services/auth.service";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
+import { MemberListComponent } from "./member-list/member-list.component";
+import { ListsComponent } from "./lists/lists.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { appRoutes } from "./routes";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
