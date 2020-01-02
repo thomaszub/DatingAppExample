e15af058458447e15efc134b0fb203b796536968
Thomas Zub <thomas.zub@codecentric.de>
2020-01-02 15:36:41 +0100

    Added member-detail stub

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MessagesComponent } from "./messages/messages.component";
 import { appRoutes } from "./routes";
 import { UserService } from "./_services/user.service";
 import { MemberCardComponent } from "./members/member-card/member-card.component";
+import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
 
 export function tokenGetter() {
   return localStorage.getItem("token");
@@ -32,7 +33,8 @@ export function tokenGetter() {
     MemberListComponent,
     ListsComponent,
     MessagesComponent,
-    MemberCardComponent
+    MemberCardComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
